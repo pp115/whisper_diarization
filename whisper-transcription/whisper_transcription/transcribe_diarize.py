9b7b35d2844959c23c8b5e74a247f4c171f1b95c
pp115 <priyampatel115@gmail.com>
2024-10-23 15:45:53 -0400

    Added example output

--- whisper-transcription/whisper_transcription/transcribe_diarize.py
+++ whisper-transcription/whisper_transcription/transcribe_diarize.py
@@ -1,3 +1,4 @@
+import os
 import whisper
 from pyannote.audio import Pipeline
 from whisper_utils import diarize_text
@@ -5,9 +6,10 @@ from pydub import AudioSegment
 import torch
 import datetime
 
-
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(device)
+
+hf_token = os.environ['HF_TOKEN']
 
 # sound = AudioSegment.from_mp3("../data/Frog_Star_Trek_NPR.mp3")
 # sound.export("../data/Frog_Star_Trek_NPR.wav", format="wav")
@@ -19,16 +21,11 @@ asr_result = model.transcribe("../data/Frog_Star_Trek_NPR.wav")
 diarization_result = pipeline("../data/Frog_Star_Trek_NPR.wav")
 final_result = diarize_text(asr_result, diarization_result)
 
-for seg, spk, sent in final_result:
-    start = str(datetime.timedelta(seconds=seg.start)).split(".")[0]
-    end = str(datetime.timedelta(seconds=seg.end)).split(".")[0]
-    line = f'{start} {end} {spk} {sent}'
-    print(line)
+with open('../data/output.txt', 'w') as file:
+    for seg, spk, sent in final_result:
+        start = str(datetime.timedelta(seconds=seg.start)).split(".")[0]
+        end = str(datetime.timedelta(seconds=seg.end)).split(".")[0]
+        line = f'{start} {end} {spk} {sent}'
+        file.write(line + "\n")
+        print(line)
 
-import importlib
-print(importlib.__version__)
-
-config = utils.backends._set_configs_from_environment()
-
-
-str(datetime.timedelta(seconds=final_result[20][0].end)).split(".")[0]
